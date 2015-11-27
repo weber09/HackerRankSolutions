5108d54513738a99260862251d9374780be20919
Gabriel Weber <gabrieldsweber@gmail.com>
2015-11-27 13:27:12 -0200

    Create TimeConversion.fs

---
+++ Domains/Algorithms/Warmup/TimeConversion.fs
@@ -0,0 +1,13 @@
+//Enter your code here. Read input from STDIN. Print output to STDOUT
+open System
+
+[<EntryPoint>]
+let main args =
+    let timeArray = Console.ReadLine().Split(':')
+    let timeDay = timeArray.[2].Substring(2)
+    let hour = timeArray.[0] |> int
+    if hour = 12 && timeDay.Equals("AM") then
+        printfn "%s:%s:%s" "00" timeArray.[1] (timeArray.[2].Substring(0,2))
+    else if hour <> 12 && timeDay.Equals("PM") then
+        printfn "%d:%s:%s" (hour + 12) timeArray.[1] (timeArray.[2].Substring(0,2))        
+    0
